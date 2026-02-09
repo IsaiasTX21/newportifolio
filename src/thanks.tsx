adff6dae4e1454761bc912909835d112c1035c0c
IsaiasTX21 <isaiascaetanodev@gmail.com>
2026-02-09 17:14:23 -0300

    portifolio2026

--- src/thanks.tsx
+++ src/thanks.tsx
@@ -4,7 +4,7 @@ export function Thanks() {
     return (
         <div id="thanks">
         <h1>nviado com successo!</h1>
-        <Link to={"https://portifolio-2026-wheat.vercel.app"}> <button>Voltar a página</button></Link>
+        <Link to={"https://portifolio2026007.netlify.app/"}> <button>Voltar a página</button></Link>
         
         </div>
     )
